3e337f37d4d5af9e4dcfff415d63c88b5bf5e891
Joele Robbins <joelerobbins@yahoo.com>
2022-10-11 19:12:17 -0500

    Adding battle, dino, robo to repository.

---
+++ battle.py
@@ -0,0 +1,8 @@
+from robo import Robot
+from dino import Dinosaur 
+
+class Battlefield:
+
+    def __init__ (self):
+        self.laser_beams = self.health - self.r_attack_power 
+        print (self.laser_beams)
