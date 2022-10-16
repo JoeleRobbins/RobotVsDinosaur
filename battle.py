0719f8e8f4b8732caddf20caff63d3e79e2c8ddc
Joele Robbins <joelerobbins@yahoo.com>
2022-10-16 16:41:32 -0500

    Attempting to fix error with importing, trying to combine all classes into one file.

--- battle.py
+++ battle.py
@@ -1,8 +1,33 @@
-from robo import Robot
-from dino import Dinosaur 
+class Weapon:
 
+    def __init__(self, name):
+        self.weapon_name = name
+        self.attack_power = 25
+        
+class Robot:
+
+    def __init__(self, name):
+        self.robo_name = name
+        self.robo_health = 200
+        self.active_weapon = self.weapon_name
+
+class Dinosaur:
+
+    def __init__(self, name):
+        self.name = name
+        self.health = 200
+        self.attack_power = 25
+        
+        
 class Battlefield:
 
-    def __init__ (self):
-        self.laser_beams = self.health - self.r_attack_power 
-        print (self.laser_beams)
+        def __init__ (self):
+            self.attack = robot_health - dinosaur_attack
+
+        def battling (self):
+            if robot_health > 0:
+                self.attack
+            else:
+                print ("The Dinosaur has won!")
+
+        
---
+++ main.py
@@ -0,0 +1,8 @@
+from battle import Battlefield
+
+print ("The battle has begun!")
+
+attacking = Battlefield.battling
+
+print (attacking)
+
